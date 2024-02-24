2f97de9dc66a8f8e00c9e45376fcbe8437679e69
Riddhi-Chatterjee <riddhich@rediffmail.com>
2024-02-25 01:24:03 +0530

    modified user controllers

--- UserManagement/src/main/java/RadVeda/UserManagement/Users/LabStaff/user/LabStaffController.java
+++ UserManagement/src/main/java/RadVeda/UserManagement/Users/LabStaff/user/LabStaffController.java
@@ -1,20 +1,46 @@
 package radveda.usermanagement.Users.LabStaff.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import radveda.usermanagement.exception.InvalidInputFormatException;
+import radveda.usermanagement.exception.UserNotFoundException;
+import radveda.usermanagement.security.UserManagementDetailsService;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/labstaffs")
 public class LabStaffController {
-    private final LabStaffService labstaffService;
+    private final LabStaffService labStaffService;
 
-    @GetMapping
-    public List<LabStaff> getLabStaffs() {
-        return labstaffService.getLabStaffs();
+    @GetMapping("/profile")
+    public LabStaff getProfile(@AuthenticationPrincipal UserDetails userDetails) throws InvalidInputFormatException, UserNotFoundException {
+        String roleEmail = userDetails.getUsername();
+
+        //Splitting the string based on the leftmost occurrence of the delimiter
+        String[] parts = roleEmail.split(UserManagementDetailsService.getDelimiter(), 2);
+
+        String email = "";
+
+        if(parts.length == 2)
+        {
+            email = parts[1];
+        }
+        else
+        {
+            throw new InvalidInputFormatException("Can't extract role and email");
+        }
+
+        Optional<LabStaff> labstaff = labStaffService.findByEmail(email);
+        if (labstaff.isEmpty()) {
+            throw new UserNotFoundException("Unable to fetch user details");
+        }
+
+        return labstaff.get();
     }
 }
--- UserManagement/src/main/java/RadVeda/UserManagement/Users/Patient/user/PatientController.java
+++ UserManagement/src/main/java/RadVeda/UserManagement/Users/Patient/user/PatientController.java
@@ -1,11 +1,16 @@
 package radveda.usermanagement.Users.Patient.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import radveda.usermanagement.exception.InvalidInputFormatException;
+import radveda.usermanagement.exception.UserNotFoundException;
+import radveda.usermanagement.security.UserManagementDetailsService;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +18,29 @@ import java.util.List;
 public class PatientController {
     private final PatientService patientService;
 
-    @GetMapping
-    public List<Patient> getPatients() {
-        return patientService.getPatients();
+    @GetMapping("/profile")
+    public Patient getProfile(@AuthenticationPrincipal UserDetails userDetails) throws InvalidInputFormatException, UserNotFoundException {
+        String roleEmail = userDetails.getUsername();
+
+        //Splitting the string based on the leftmost occurrence of the delimiter
+        String[] parts = roleEmail.split(UserManagementDetailsService.getDelimiter(), 2);
+
+        String email = "";
+
+        if(parts.length == 2)
+        {
+            email = parts[1];
+        }
+        else
+        {
+            throw new InvalidInputFormatException("Can't extract role and email");
+        }
+
+        Optional<Patient> patient = patientService.findByEmail(email);
+        if (patient.isEmpty()) {
+            throw new UserNotFoundException("Unable to fetch user details");
+        }
+
+        return patient.get();
     }
 }
