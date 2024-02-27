968283bbf323597c48c2235b6dae9d630fb618b8
Ankrutee <79003737+Ankrutee@users.noreply.github.com>
2024-02-27 16:41:43 +0530

    Update Patient.java

--- UserManagement/src/main/java/RadVeda/UserManagement/Users/Patient/user/Patient.java
+++ UserManagement/src/main/java/RadVeda/UserManagement/Users/Patient/user/Patient.java
@@ -1,9 +1,10 @@
 package RadVeda.UserManagement.Users.Patient.user;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import RadVeda.UserManagement.Users.User.User;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -14,22 +15,34 @@ public class Patient extends User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String orgName;
-    private String orgAddressL1;
-    private String orgAddressL2;
+    // Existing fields
+    private String dateOfBirth;
+    private String gender;
+    private String ethnicity;
+    private String maritalStatus;
+    private String race;
+
+    // New guardian fields
+    private String guardianName;
+    private String guardianRelationship;
+    private String guardianPhoneNumber;
+    private String guardianEmailAddress;
 
     public Patient() {
         this.setRole("PATIENT");
     }
 
-    public Patient(String firstName, String middleName, String lastName, String addressL1, String addressL2,
-            String country, String state, String city, String email, String password, String phoneNumber, String role,
-            boolean isEnabled, String orgName, String orgAddressL1, String orgAddressL2) {
-        super(firstName, middleName, lastName, addressL1, addressL2, country, state, city, email, password, phoneNumber,
-                role, isEnabled);
-        this.orgName = orgName;
-        this.orgAddressL1 = orgAddressL1;
-        this.orgAddressL2 = orgAddressL2;
+    public Patient(String firstName, String middleName, String lastName, String addressL1, String addressL2, String country, String state, String city, String email, String password, String phoneNumber, String role, boolean isEnabled, String dateOfBirth, String gender, String ethnicity, String maritalStatus, String race, String guardianName, String guardianRelationship, String guardianPhoneNumber, String guardianEmailAddress) {
+        super(firstName, middleName, lastName, addressL1, addressL2, country, state, city, email, password, phoneNumber, role, isEnabled);
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.ethnicity = ethnicity;
+        this.maritalStatus = maritalStatus;
+        this.race = race;
+        this.guardianName = guardianName;
+        this.guardianRelationship = guardianRelationship;
+        this.guardianPhoneNumber = guardianPhoneNumber;
+        this.guardianEmailAddress = guardianEmailAddress;
         this.setRole("PATIENT");
     }
 }
