4af9c66ad8da0e7d1e34cd09a8e96e5fd5be1538
Ankrutee <79003737+Ankrutee@users.noreply.github.com>
2024-02-27 16:42:53 +0530

    Update PatientSignUpRequest.java

--- UserManagement/src/main/java/RadVeda/UserManagement/Users/Patient/signup/PatientSignUpRequest.java
+++ UserManagement/src/main/java/RadVeda/UserManagement/Users/Patient/signup/PatientSignUpRequest.java
@@ -1,18 +1,24 @@
 package RadVeda.UserManagement.Users.Patient.signup;
 
 public record PatientSignUpRequest(
-                String firstName,
-                String middleName,
-                String lastName,
-                String addressL1,
-                String addressL2,
-                String country,
-                String state,
-                String city,
-                String email,
-                String password,
-                String phoneNumber,
-                String orgName,
-                String orgAddressL1,
-                String orgAddressL2) {
+        String firstName,
+        String middleName,
+        String lastName,
+        String addressL1,
+        String addressL2,
+        String country,
+        String state,
+        String city,
+        String email,
+        String password,
+        String phoneNumber,
+        String dateOfBirth,
+        String gender,
+        String ethnicity,
+        String maritalStatus,
+        String race,
+        String guardianName,
+        String guardianRelationship,
+        String guardianPhoneNumber,
+        String guardianEmailAddress) {
 }
